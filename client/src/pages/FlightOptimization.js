c533d931f6da74f6a593dd61ee910aa9fd42f433
Danial-Hussain <ali.danial.hussain@gmail.com>
2022-04-06 03:10:37 -0400

    make flight optimization page dynamic

--- client/src/pages/FlightOptimization.js
+++ client/src/pages/FlightOptimization.js
@@ -1,46 +1,124 @@
-import React from "react";
+import React, { useState } from "react";
+import { states } from "../utils/constants";
 import { Navbar } from "../components/Navbar";
+import { arrayMove } from "react-sortable-hoc";
 import { ReactComponent as Info } from "../icons/info.svg";
 import { ReactComponent as Search } from "../icons/search.svg";
 import { TrendLineChart } from "../components/TrendLineChart";
+import { SortableContainer, SortableElement } from "react-sortable-hoc";
+import { formatData, getCities, getMonth } from "../utils/helpers";
+
+const SortableItem = SortableElement(({ value }) => (
+  <li className="bg-gray-50 p-2 mb-1 text-gray-600">{value}</li>
+));
+
+const SortableList = SortableContainer(({ items }) => {
+  return (
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${value}`} index={index} value={value} />
+      ))}
+    </ul>
+  );
+});
 
 export const FlightOption = ({
   rank,
   airline,
+  airport,
+  city,
   lateAircraftDelay,
   weatherDelay,
   securityDelay,
+  nasDelay,
+  carrierDelay,
   totalDelay,
+  delays,
 }) => {
+  const month = getMonth();
   return (
-    <div className="border-solid border-[0.1px] border-gray-300 rounded-md p-2 flex flex-row justify-between hover:shadow-md w-full h-56">
-      <div className="flex flex-col w-1/2 p-2">
-        <h1 className="text-gray-800 text-lg font-semibold">
+    <div className="border-solid border-[0.1px] border-gray-300 rounded-md p-2 flex flex-row justify-between items-center hover:shadow-md w-full h-fit">
+      <div className="flex flex-col w-1/2 pl-4 p-1">
+        <div className="text-gray-800 text-lg font-semibold pb-2">
           {rank}. {airline}
-        </h1>
+        </div>
+        <div className="text-gray-800 text-sm">
+          {airport} - {city}
+        </div>
+        <div className="pt-2 underline">Average Stats for {month}</div>
         <ul className="text-gray-500">
           <li>- Late Aircraft Delay: {lateAircraftDelay} min.</li>
           <li>- Weather Delay: {weatherDelay} min.</li>
           <li>- Security Delay: {securityDelay} min.</li>
-          <li>- Total Delay: {totalDelay} hr.</li>
+          <li>- NAS Delay: {nasDelay} min.</li>
+          <li>- Carrier Delay: {carrierDelay} min.</li>
+          <li>- Total Delay: {totalDelay} min.</li>
         </ul>
       </div>
-      <TrendLineChart strokeWidth={1} />
+      <TrendLineChart
+        strokeWidth={2}
+        data={delays}
+        xkey={"Month"}
+        ykeys={["TotalDelay"]}
+      />
     </div>
   );
 };
 
 export const FlightOptimization = () => {
-  const sortingParams = [
+  const [data, setData] = useState({});
+  const [cities, setCities] = useState([]);
+  const [city, setCity] = useState("");
+  const [placeholder, setPlaceholder] = useState("Select a state to find data");
+
+  const [sortingParams, setSortingParams] = useState([
     "Late Aircraft Delay",
     "Weather Delay",
     "NAS Delay Delay",
     "Security Delay",
     "Carrier Delay",
-  ];
+  ]);
+
+  const getData = async (state) => {
+    setData({});
+    setCity("");
+    setCities([]);
+    setPlaceholder("Loading...");
+
+    const month = new Date().getUTCMonth() + 1;
+
+    const resp = await fetch("http://localhost:5000/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({ queryType: 2, state: state, month: month }),
+    });
+
+    const json = await resp.json();
+    const data = formatData(json, month);
+    const cities = getCities(data);
+
+    setCities(cities);
+    setData(data);
+  };
+
+  const filterData = (data) => {
+    return Object.fromEntries(
+      Object.entries(data).filter(
+        ([key, value]) => value.City === city || city == ""
+      )
+    );
+  };
+
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    setSortingParams(({ items }) => ({
+      items: arrayMove(items, oldIndex, newIndex),
+    }));
+  };
 
   return (
-    <>
+    <div className="overflow-hidden">
       <Navbar />
       <div className="container mx-auto max-w-6xl flex flex-col gap-4 pt-12 p-4">
         <div className="container mx-auto max-w-sm">
@@ -69,36 +147,73 @@ export const FlightOptimization = () => {
                   Average Delay Graph
                 </h1>
               </div>
-              <FlightOption
-                rank={1}
-                airline={"Jet Blue Airline"}
-                lateAircraftDelay={"26"}
-                weatherDelay={"14"}
-                securityDelay={"20"}
-                totalDelay={"1"}
-              />
-              <FlightOption
-                rank={2}
-                airline={"Southwest Airlines"}
-                lateAircraftDelay={"26"}
-                weatherDelay={"14"}
-                securityDelay={"20"}
-                totalDelay={"1"}
-              />
+              {Object.keys(data).length === 0 ? (
+                <div className="bg-gray-50 h-60 flex flex-col items-center justify-center rounded-md">
+                  <h1 className="text-2xl text-gray-600">{placeholder}</h1>
+                </div>
+              ) : (
+                <>
+                  {Object.keys(filterData(data))
+                    .slice(0, 10)
+                    .map((d, i) => (
+                      <FlightOption
+                        key={i}
+                        rank={i + 1}
+                        airline={data[d].Airline}
+                        airport={data[d].Airport}
+                        city={data[d].City}
+                        lateAircraftDelay={data[d].LateAircraftDelayThisMonth}
+                        weatherDelay={data[d].WeatherDelayThisMonth}
+                        securityDelay={data[d].SecurityDelayThisMonth}
+                        nasDelay={data[d].NASDelayThisMonth}
+                        carrierDelay={data[d].CarrierDelayThisMonth}
+                        totalDelay={data[d].TotalDelayThisMonth}
+                        delays={data[d].Delays}
+                      />
+                    ))}
+                </>
+              )}
             </div>
           </div>
           <div className="col-span-1 border-l border-1 pr-2 pl-3">
             <div className="inline-flex min-w-full items-center">
-              <input
-                className="border-b border-1 text-gray-500 flex-grow focus:outline-none focus:border-gray-500 focus:border-b-2 p-1"
-                placeholder="Your city"
-              />
+              <select
+                className="border-b border-1 text-gray-500 flex-grow focus:outline-none focus:border-gray-500 focus:border-b-2 p-1 mr-4"
+                onChange={(e) =>
+                  e.target.value !== "" ? getData(e.target.value) : null
+                }
+              >
+                <option value="">Select a state</option>
+                {states.map((s, i) => (
+                  <option value={s} key={i}>
+                    {s}
+                  </option>
+                ))}
+              </select>
               <Search className="w-4 h-4 stroke-gray-500" />
             </div>
-            <input
-              className="border-b border-1 min-w-full text-gray-500 text-right pt-2 focus:outline-none focus:border-gray-500 focus:border-b-2 p-1"
-              placeholder="Radius mi."
-            />
+            {cities.length === 0 ? (
+              <select
+                className="min-w-full border-b border-1 text-gray-500 flex-grow focus:outline-none focus:border-gray-500 focus:border-b-2 p-1 mr-4"
+                disabled
+              >
+                <option value="">Select a city</option>
+              </select>
+            ) : (
+              <select
+                className="min-w-full border-b border-1 text-gray-500 flex-grow focus:outline-none focus:border-gray-500 focus:border-b-2 p-1 mr-4"
+                onChange={(e) =>
+                  e.target.value !== "" ? setCity(e.target.value) : null
+                }
+              >
+                <option value="">Select a city</option>
+                {cities.map((c, i) => (
+                  <option value={c} key={i}>
+                    {c}
+                  </option>
+                ))}
+              </select>
+            )}
             <div className="bg-indigo-600 rounded-md p-2 inline-flex mt-4">
               <Info className="w-7 h-7 stroke-gray-50" />
               <p className="text-gray-100 pl-2">
@@ -109,17 +224,10 @@ export const FlightOptimization = () => {
             <h1 className="text-xl text-gray-800 font-bold mt-4">
               Sorting by lowest
             </h1>
-            {sortingParams.map((param, i) => {
-              return (
-                <div className="flex flex-row gap-2 items-center mt-2" key={i}>
-                  <input type="checkbox" className="w-4 h-4" />
-                  <h1 className="text-lg">{param}</h1>
-                </div>
-              );
-            })}
+            <SortableList items={sortingParams} onSortEnd={onSortEnd} />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
---
+++ client/src/utils/helpers.js
@@ -0,0 +1,71 @@
+export const formatData = (data, month) => {
+  let currentAirline = "";
+  let currentAirport = "";
+  let key = "";
+  const newData = {};
+
+  data.map((d, i) => {
+    if (d.AIRLINE !== currentAirline || d.AIRPORT !== currentAirport) {
+      currentAirline = d.AIRLINE;
+      currentAirport = d.AIRPORT;
+      key = `${currentAirline}-${currentAirport}`;
+      newData[key] = {
+        Airline: d.AIRLINE,
+        Airport: d.AIRPORT.split(":")[1],
+        City: d.AIRPORT.split(":")[0],
+        Delays: [],
+        TotalDelayThisMonth: 0,
+        LateAircraftDelayThisMonth: 0,
+        NASDelayThisMonth: 0,
+        SecurityDelayThisMonth: 0,
+        WeatherDelayThisMonth: 0,
+        CarrierDelayThisMonth: 0,
+      };
+    }
+
+    newData[key].Delays.push({
+      Month: d["FLIGHTMONTH"],
+      TotalDelay: Math.round(d["AVGTOTALDELAY"]),
+    });
+
+    if (d["FLIGHTMONTH"] === month) {
+      newData[key]["TotalDelayThisMonth"] = Math.round(d["AVGTOTALDELAY"]);
+      newData[key]["LateAircraftDelayThisMonth"] = Math.round(
+        d["LATEAIRCRAFTDELAY"]
+      );
+      newData[key]["NASDelayThisMonth"] = Math.round(d["NASDELAY"]);
+      newData[key]["SecurityDelayThisMonth"] = Math.round(d["SECURITYDELAY"]);
+      newData[key]["WeatherDelayThisMonth"] = Math.round(d["WEATHERDELAY"]);
+      newData[key]["CarrierDelayThisMonth"] = Math.round(d["CARRIERDELAY"]);
+    }
+  });
+  return newData;
+};
+
+export const getCities = (data) => {
+  let cities = new Set();
+  Object.keys(data).map((d) => {
+    cities.add(data[d].City);
+  });
+  return Array.from(cities);
+};
+
+export const getMonth = () => {
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const d = new Date();
+  let name = month[d.getMonth()];
+  return name;
+};
